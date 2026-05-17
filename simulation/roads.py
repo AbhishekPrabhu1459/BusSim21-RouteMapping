63a21193bd884c91b809f0a5c67d926c83048bbf
ABHISHEK <abhipro1011@gmail.com>
2026-05-17 16:43:27 +1000

    [BS21] Adding and creating simul files - one

---
+++ simulation/roads.py
@@ -0,0 +1,6 @@
+class Road:
+    def __init__(self, from_stop, to_stop, travel_time, traffic_level):
+        self.from_stop = from_stop
+        self.to_stop = to_stop
+        self.travel_time = travel_time
+        self.traffic_level = traffic_level
---
+++ simulation/stops.py
@@ -0,0 +1,4 @@
+class BusStop:
+    def __init__(self, name, zone_type):
+        self.name = name
+        self.zone_type = zone_type
